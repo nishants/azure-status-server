f4c396d34dd0a6d20ee8f41354aeda8febd5f21b
nishant <nishant.singh87@gmail.com>
2019-11-21 23:09:17 +0530

    listen to updated pull requests

--- routes/pullRequests.js
+++ routes/pullRequests.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const db = {
+  'created': [],
+  'updated': []
+};
+
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(db);
 });
 router.post('/webhooks/created', function(request, response, next) {
   const reponseBody = {
     message: "Pull request is created",
     data: request.body
   };
-  console.log(reponseBody)
+  db.created.push(request.body);
   response.send(reponseBody);
 });
+
+router.post('/webhooks/updated', function(request, response, next) {
+  const reponseBody = {
+    message: "Pull request is updated",
+    data: request.body
+  };
+  db.updated.push(request.body);
+  response.send(reponseBody);
+});
+
 
 module.exports = router;
